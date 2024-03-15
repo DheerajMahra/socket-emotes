e1d26774aa974b66d356998f53e99b13436387a5
Dheeraj Mahra <dheeraj.mahra@cars24.com>
2024-03-15 23:36:34 +0530

    feat: resolve websocket endpoint based on environment

--- client/src/constants/api-constants.ts
+++ client/src/constants/api-constants.ts
@@ -1,1 +1,7 @@
-export const wsEndpoint = "ws://localhost:8080";
+export const wsEndpoint = (function() {
+  if(process.env.NODE_ENV === "production") {
+    const protocol = window.location.protocol.includes('https') ? 'wss': 'ws';
+    return `${protocol}://${window.location.host}`;
+  }
+  return "ws://localhost:8080";
+})();
